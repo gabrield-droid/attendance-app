3fe6887be0ec17890697220bdf17fd7dea74a888
Gabriel Dylan <gabrieldylan474@gmail.com>
2025-03-21 19:20:25 +0800

    try fixing bugs3

--- library/config.php
+++ library/config.php
@@ -3,8 +3,9 @@
     $user = "admin";
     $pass = "admin";
     $db = "db_attendance";
+    $port = 3306;
 
-    $con = mysqli_connect($host, $user,$pass, $db);
+    $con = mysqli_connect($host, $user,$pass, $db, $port);
     if (mysqli_connect_errno()) {
         echo "Koneksi gagal: ".mysqli_connect_errno();
     }
