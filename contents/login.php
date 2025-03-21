afac86b68756f7d5708a5a63b1302a634b054800
Gabriel Dylan <gabrieldylan474@gmail.com>
2025-03-21 19:26:42 +0800

    try fixing bugs5

--- contents/login.php
+++ contents/login.php
@@ -1,7 +1,7 @@
 <a href="index.php">Kembali</a>
 <section class="login_box">
     <h2>Login Aplikasi</h2>
-    <form method="post" action="?content=process_login.php">
+    <form method="post" action="?content=process_login">
         <input type="text" placeholder="Username" name="username">
         <input type="password" placeholder="Password" name="password">
         <input type="submit" value="Login">
