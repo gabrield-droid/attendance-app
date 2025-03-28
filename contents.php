268bd19b437f952e6d2f881319feb05cf3d08760
Gabriel Dylan <gabrieldylan474@gmail.com>
2025-03-28 14:44:04 +0800

    added form actions above the form's responses

--- contents.php
+++ contents.php
@@ -1,5 +1,9 @@
 <?php
-    $contents = array("home", "login", "logout", "addform", "fill_form", "form_detail", "process_addform", "process_fill_form", "process_login");
+    $contents = array("home", "login", "logout", "addform",
+                        "fill_form", "form_detail", "process_addform",
+                        "process_fill_form", "process_login", "edit-form",
+                        "export-response-form", "copy-link", "delete-form",
+                        "process_editform");
     
     if (isset($_GET['content'])) {
         $content = $_GET['content'];
