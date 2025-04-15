c6f2c935fe92dde72f766efb51a33889fe01f121
Gabriel Dylan <gabrieldylan474@gmail.com>
2025-04-15 14:31:03 +0800

    edit the code for faster redirection to the index page

--- contents/logout.php
+++ contents/logout.php
@@ -2,6 +2,5 @@
     session_start();
     session_destroy();
 
-    //echo "<p align='center'>Anda telah logout!</p>";
-    echo "<meta http-equiv='refresh' content='0; url=index.php'>";
+    header('location: /');
 ?>
