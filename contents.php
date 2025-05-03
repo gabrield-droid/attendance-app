1a1707b59eccd201a56a4eacf2686103622e6f6e
Gabriel Dylan <gabrieldylan474@gmail.com>
2025-05-03 22:21:18 +0800

    Remove unnecessary element in the array of contents

--- contents.php
+++ contents.php
@@ -2,8 +2,8 @@
     $contents = array("home", "login", "logout", "addform",
                         "fill_form", "form_detail", "process_addform",
                         "process_fill_form", "process_login", "edit-form",
-                        "export-response-form", "delete-form",
-                        "process_editform", "process_deleteform");
+                        "delete-form", "process_editform",
+                        "process_deleteform");
     
     if (isset($_GET['content'])) {
         $content = $_GET['content'];
